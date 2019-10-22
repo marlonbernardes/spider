35be9b895f5c8203656dd6e84266d8143d811da7
Marlon Bernardes <marlonjb.cb@gmail.com>
2019-10-22 11:00:55 +0100

    feat: (WIP) implement crawling scheduler

--- agent/src/runner.ts
+++ agent/src/runner.ts
@@ -1,35 +1,44 @@
 import Crawler from './crawler'
 import { DefaultHttpClient } from './lib/http'
-import { HtmlParser } from './lib/parser'
+import { save } from './es'
+import { CrawlingQueue, InMemoryCrawlingQueue } from './lib/queue'
+import { VisitedPagesCache, RedisCache } from './lib/cache'
 
-// @TODO Move to kafka
-let toCrawl: string[] = ['https://monzo.com']
-
-// @TODO Move to redis
-const cached: string[] = []
-
+const cache: VisitedPagesCache = new RedisCache()
+const queue: CrawlingQueue = new InMemoryCrawlingQueue()
 const http = new DefaultHttpClient()
-const parser = new HtmlParser()
+const crawler = new Crawler(http)
 
-const crawler = new Crawler(http, {
-  'text/html': parser
-})
+async function process (url: string) {
+  try {
+    if (await cache.isVisited(url)) {
+      console.log(`skipping ${url} as it's already cached`)
+    } else {
+      const response = await crawler.crawl(url)
+      console.log(`finished crawling ${url}`)
+
+      await save({
+        textContent: response.textContent,
+        timestamp: new Date()
+      })
+
+      await cache.markAsVisited(url)
+
+      for (let childUrl of response.links) {
+        if (!await cache.isVisited(childUrl)) {
+           await queue.add([childUrl])
+        }
+      }
+    }
+  } catch (e) {
+    console.log(`error crawling ${url}`, e.message)
+  }
+}
 
 export async function run () {
-  while (toCrawl.length > 0) {
-    const [url] = toCrawl.splice(0, 1)
-    try {
-      if (cached.includes(url)) {
-        console.log(`skipping ${url} as it's already cached`)
-      } else {
-        const response = await crawler.crawl(url)
-        console.log(`finished crawling ${url}`, toCrawl.length)
-        cached.push(url)
-        toCrawl = toCrawl.concat(response.links)
-      }
-    } catch (e) {
-      console.log(`error crawling ${url}`)
-    }
-  }
+  // @TODO remove cache clear
+  await cache.clear()
+  await queue.add(['http://codeheaven.io'])
+  await queue.onMessage(process)
 }
 
