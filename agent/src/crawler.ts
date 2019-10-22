daa572e50fc409fa68b657f8f10390ccd721350c
Marlon Bernardes <marlonjb.cb@gmail.com>
2019-10-22 10:55:37 +0100

    refactor: add default parsers to crawler

--- agent/src/crawler.ts
+++ agent/src/crawler.ts
@@ -1,21 +1,31 @@
 import { HttpClient } from './lib/http'
 import { Parser, NoOpParser } from './lib/parser'
 import settings from './config/settings'
+import { HtmlParser } from './lib/parser'
 
 export type Parsers = {
   [contentType: string]: Parser
+}
+
+const DEFAULT_PARSERS = {
+  'text/html': new HtmlParser()
 }
 
 export default class Crawler {
 
   static NO_OP_PARSER = new NoOpParser()
 
-  constructor (private http: HttpClient, private parsers: Parsers) {}
+  parsers: Parsers
+
+  constructor (private http: HttpClient, parsers: Parsers = DEFAULT_PARSERS) {
+    this.parsers = Object.assign({}, DEFAULT_PARSERS, parsers)
+  }
 
   async crawl (url: string) {
     const response = await this.http.get(url)
     const { host, protocol } = new URL(url)
     const parser = this.parsers[response.contentType] || Crawler.NO_OP_PARSER
+
     return parser.parse(response.content, {
       linksSelector: settings.linksSelector,
       baseDomain: `${protocol}//${host}`,
